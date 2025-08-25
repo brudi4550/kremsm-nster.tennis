b115064bc2aabf7d0bbde1d7cc1353a8c0539a26
Alexander Wolf <alexanderwolf@outlook.at>
2025-08-25 10:14:38 +0200

    test

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,4 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   output: 'export', // enables `next export`
-  basePath: isProd ? '/kremsmuenster-tennis' : '',
-  assetPrefix: isProd ? '/kremsmuenster-tennis/' : '',
 };
